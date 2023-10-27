29e9b71cb5f4f7df0c2f4a1fb6c8d733319cfe92
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-26 23:29:47 -0300

    ejemplo de authrequired

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,7 +14,11 @@ import { updateProfile } from "../controllers/users.controller.js";
 const router = Router();
 
 router.post("/login", validateSchema(loginSchema), loginUser);
-router.post("/logout", logoutUser);
+router.post("/logout",  logoutUser);
+// si quieres probar el authrequired que valida si está logeado con nextauth 
+// debes descomentar la linea y ejecutar el login de la aplicación
+// ***en local***
+// router.post("/login", authrequired, validateSchema(loginSchema), loginUser);
 router.post("/register", validateSchema(registerSchema), registerUser);
 router.patch("/users/:id", updateProfile);
 router.get("/confirm/:token", confirmUser);
